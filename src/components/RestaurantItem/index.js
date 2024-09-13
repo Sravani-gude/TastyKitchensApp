ba4d4d329b2b4eecd87529d375e98f42e654c714
Gude Sravani <109786685+Sravani-gude@users.noreply.github.com>
2024-09-13 10:03:35 +0530

    Update index.js

--- src/components/RestaurantItem/index.js
+++ src/components/RestaurantItem/index.js
@@ -5,7 +5,8 @@ import './index.css'
 const RestaurentItem = props => {
   const {list} = props
   const {id, imgUrl, name, cuisine, userRating} = list
-  const {rating} = userRating
+  const {rating, totalReviews} = userRating // Added totalReviews
+
   return (
     <Link to={`/restaurant/${id}`} className="restaurant-item-nav-link">
       <li className="Restaurant-list-item" data-testid="restaurant-item">
@@ -21,6 +22,8 @@ const RestaurentItem = props => {
             <div className="rating">
               <BsFillStarFill color="#F7931E" />
               <p>{rating}</p>
+              <p className="total-reviews">({totalReviews} reviews)</p>{' '}
+              {/* Displaying total reviews */}
             </div>
           </div>
         </div>
@@ -28,4 +31,63 @@ const RestaurentItem = props => {
     </Link>
   )
 }
+
 export default RestaurentItem
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import {BsFillStarFill} from 'react-icons/bs'
+// import {Link} from 'react-router-dom'
+// import './index.css'
+
+// const RestaurentItem = props => {
+//   const {list} = props
+//   const {id, imgUrl, name, cuisine, userRating} = list
+//   const {rating} = userRating
+//   return (
+//     <Link to={`/restaurant/${id}`} className="restaurant-item-nav-link">
+//       <li className="Restaurant-list-item" data-testid="restaurant-item">
+//         <div className="restaurant-details">
+//           <img
+//             src={imgUrl}
+//             alt="restaurant"
+//             className="restaurant-image-width"
+//           />
+//           <div className="restaurant-description">
+//             <h1 className="restaurant-name">{name}</h1>
+//             <p className="cuisine-type">{cuisine}</p>
+//             <div className="rating">
+//               <BsFillStarFill color="#F7931E" />
+//               <p>{rating}</p>
+//             </div>
+//           </div>
+//         </div>
+//       </li>
+//     </Link>
+//   )
+// }
+// export default RestaurentItem
