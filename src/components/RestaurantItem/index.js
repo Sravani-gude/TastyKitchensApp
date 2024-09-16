482c606da397e6513c2f7c40c02b80a51dd5878a
Gude Sravani <109786685+Sravani-gude@users.noreply.github.com>
2024-09-16 18:57:04 +0530

    Update index.js

--- src/components/RestaurantItem/index.js
+++ src/components/RestaurantItem/index.js
@@ -5,7 +5,7 @@ import './index.css'
 const RestaurentItem = props => {
   const {list} = props
   const {id, imgUrl, name, cuisine, userRating} = list
-  const {rating, totalReviews} = userRating // Added totalReviews
+  const {rating, total_reviews} = userRating // Corrected totalReviews to total_reviews
 
   return (
     <Link to={`/restaurant/${id}`} className="restaurant-item-nav-link">
@@ -22,8 +22,8 @@ const RestaurentItem = props => {
             <div className="rating">
               <BsFillStarFill color="#F7931E" />
               <p>{rating}</p>
-              <p className="total-reviews">({totalReviews} reviews)</p>{' '}
-              {/* Displaying total reviews */}
+              <p className="total-reviews">({total_reviews} reviews)</p>{' '}
+              {/* Updated total_reviews */}
             </div>
           </div>
         </div>
@@ -33,6 +33,65 @@ const RestaurentItem = props => {
 }
 
 export default RestaurentItem
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import {BsFillStarFill} from 'react-icons/bs'
+// import {Link} from 'react-router-dom'
+// import './index.css'
+
+// const RestaurentItem = props => {
+//   const {list} = props
+//   const {id, imgUrl, name, cuisine, userRating} = list
+//   const {rating, totalReviews} = userRating // Added totalReviews
+
+//   return (
+//     <Link to={`/restaurant/${id}`} className="restaurant-item-nav-link">
+//       <li className="Restaurant-list-item" data-testid="restaurant-item">
+//         <div className="restaurant-details">
+//           <img
+//             src={imgUrl}
+//             alt="restaurant"
+//             className="restaurant-image-width"
+//           />
+//           <div className="restaurant-description">
+//             <h1 className="restaurant-name">{name}</h1>
+//             <p className="cuisine-type">{cuisine}</p>
+//             <div className="rating">
+//               <BsFillStarFill color="#F7931E" />
+//               <p>{rating}</p>
+//               <p className="total-reviews">({totalReviews} reviews)</p>{' '}
+//               {/* Displaying total reviews */}
+//             </div>
+//           </div>
+//         </div>
+//       </li>
+//     </Link>
+//   )
+// }
+
+// export default RestaurentItem
 
 
 
